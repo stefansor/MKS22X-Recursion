237a44f6a242cdb833b31dfecf62208eacef0b7d
stefansor <ssorobay00@stuy.edu>
2019-02-03 16:51:18 -0500

    added make all sums and helper function file compiles still must test

--- Recursion.java
+++ Recursion.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 
 public class Recursion{
   public static double sqrt(double n, double tolerance){
@@ -5,7 +7,7 @@ public class Recursion{
   }
 
 
-  public static double sqrtHelp(double n, double g, double tol){
+  private static double sqrtHelp(double n, double g, double tol){
     if(n == 0 || n == 1){
       return n;
     }
@@ -17,10 +19,49 @@ public class Recursion{
     }
   }
 
+
+
+  public static int fib(int num){
+    if(num == 0){
+      return 0;
+    }
+    if(num == 2 || num == 1){
+      return 1;
+    }
+    else{
+      return fibHelp(num, 1, 0);
+    }
+  }
+  private static int fibHelp(int num, int partial, int end){
+    if(num == 0){
+      return end;
+    }
+    else{
+      return fibHelp(num - 1, partial + end, partial );
+    }
+  }
+  public static ArrayList<Integer> makeAllSums(int n){
+    ArrayList<Integer> sums = new ArrayList<Integer>();
+    return makeAllSumsHelp(n, n, 0, sums);
+  }
+
+  private static ArrayList<Integer> makeAllSumsHelp(int n, int start, int sum, ArrayList<Integer> data){
+    if(start == 0){
+      data.add(sum);
+    }
+    else{
+      makeAllSumsHelp(n, start - 1, sum + start, data);
+      makeAllSumsHelp(n, start - 1, sum, data);
+    }
+    return data;
+  }
+
   public static void main(String args[]){
     System.out.println(sqrt(1, 0.01));
     System.out.println(sqrt(0, 0.01));
     System.out.println(sqrt(2, 0.01));
     System.out.println(sqrt(100, 0.01));
+    //used codingbat to test fib tail recursion//
+
   }
 }
